636cfcba1f08440db03550caa12cfbc1214b2c09
Matt Phillips <mattphillips@mail.com>
2017-02-16 12:01:43 +0000

    Linux debug break implementation

--- ion/core/Platform.h
+++ ion/core/Platform.h
@@ -12,9 +12,11 @@
 #include <Windows.h>
 #include <direct.h>
 #elif defined ION_PLATFORM_MACOSX
-#include <kos.h>
+#include <sys/time.h>
+#elif defined ION_PLATFORM_LINUX
 #include <sys/time.h>
 #elif defined ION_PLATFORM_DREAMCAST
+#include <kos.h>
 #include <time.h>
 #elif defined ION_PLATFORM_LINUX
 
